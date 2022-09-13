0d05bf11c0588a11c88184e199f7ce34dcf617e4
HP <mdzaid1299@gmail.com>
2022-09-13 13:06:35 +0530

    initial

--- src/main/java/com/jap/DeleteUpdateStudentData.java
+++ src/main/java/com/jap/DeleteUpdateStudentData.java
@@ -9,28 +9,46 @@ import java.sql.*;
 public class DeleteUpdateStudentData
 {
 
-    public Connection getConnection(){
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
         Connection connection=null;
-
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/school";
+        String user = "root";
+        String pw = "admin";
+        connection = DriverManager.getConnection(url, user, pw);
         return connection;
 
     }
 
-    public void deleteStudentData(){
+    public void deleteStudentData() throws SQLException, ClassNotFoundException {
       // call getConnection() method
-       
+        Statement st = getConnection().createStatement();
         //execute delete query
+        String sql = "Delete from student where Rollnumber = 14";
+        st.executeUpdate(sql);
+        System.err.println("Deletion Success");
+
     }
 
-    public void updateStudentData(){
+    public void updateStudentData() throws SQLException, ClassNotFoundException {
         // call getConnection() method
-
+        Statement st = getConnection().createStatement();
         //execute update query
+        String sql = "update student set age = 30 where totalmarks = 678 and Rollnumber = 15";
+        st.executeUpdate(sql);
+        System.err.println("updation success");
     }
 
     public static void main(String[] args) {
         DeleteUpdateStudentData salesDataDemo = new DeleteUpdateStudentData();
-        salesDataDemo.deleteStudentData();
-        salesDataDemo.updateStudentData();
+        try {
+            salesDataDemo.deleteStudentData();
+            salesDataDemo.updateStudentData();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
